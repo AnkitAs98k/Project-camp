324c1bf5b74a5be3277b69bc9dd69c333ef6deff
Ankit <ankitsuman2022@gmail.com>
2026-03-16 23:59:23 +0530

    project

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -1,7 +1,9 @@
 import { Project } from "../models/project.models.js";
-
+import { ProjectMember } from "../models/projectMember.models.js";
 import { asyncHandler } from "../utils/aysncHandler.js";
-
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js"
+import mongoose from "mongoose"
 
 /*const Projects = asyncHandler(async (req, res) => {
     //  `GET /`
@@ -20,47 +22,94 @@ import { asyncHandler } from "../utils/aysncHandler.js";
 const createProjects = asyncHandler(async (req, res) => {
     // `POST /
     
-        const { title, content, author } = req.body;
+        const { title, content } = req.body;
         const existedProject = await Project.findOne({ title });
         if (existedProject) {
-            req.status(404).json({ message: "project already present" })
+            req.status(400).json({ message: "project already present" })
         }
 
         const newProject = await Project.create({
             title,
             content,
-            author
+            author:new mongoose.Types.ObjectId(req.user._id)
+        });
+
+        await ProjectMember.create({
+                user: new mongoose.Types.ObjectId(req.user._id),
+                project: new mongoose.Types.ObjectId(newProject._id),
+                role: userRoleEnum.admin
+
         })
-        res.status(201).json({ newProject });
+
+        
+        res
+        .status(201)
+        .json(new ApiResponse(
+            201,
+            newProject,
+            "Project succesfully created"
+        ));
    
    
 })
+
+
+const updateProject = asyncHandler(async (req, res) => {
+    const {title, content} = req.body;
+    const projectId = req.params;
+
+    await Project.findByIdAndUpdate(
+        projectId,
+        {
+            title,
+            content
+        },
+        {new:true}
+    );
+
+    if(!projectId){
+        return res.status(404).json({message:"Id not found"});
+    }
+
+    res.status(200)
+    .json(new ApiResponse(
+        200,
+        Project,
+        "Updated succesfully"
+    )
+    )
+
+})
+
+
+const deleteProject = asyncHandler(async (req, res) => {
+    //  `DELETE /:projectId`
+
+    const {projectId} = req.params;
+    await Project.findByIdAndDelete(projectId);
+    
+
+    if(!projectId){
+        throw new ApiError(404,"project not found")
+        }
+
+    return res.status(200)
+              .json(new ApiResponse(
+                    200,
+                    "Deleted"
+              ))
+})
+
+
+
 
 const getProjectbyId = asyncHandler(async (req, res) => {
 
     //  `GET /:projectId
-    
-    // we are finding this by    
-    const projectId = req.params;
-    if(!projectId){
-        return res.status(404).json({message:"MissedID"})
-    }
-    const project = await Project.findById(projectId);
-    if(!project){
-        res.status(404).json({
-            message:"projct not  found"
-        })
-    }
-    res.status(201).json({project}) 
+
 })
 
-const updateProject = asyncHandler(async (req, res) => {
-    // PUT /:projectId
-})
 
-const deleteProject = asyncHandler(async (req, res) => {
-    //  `DELETE /:projectId`
-})
 
 
 const getProjectMembers = asyncHandler(async (req, res) => {
